0865b0d1b8c5b764ad028f0b6e3f9d14ead9908f
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2026-05-14 18:38:36 +0800

    Move static files to public/ so Vercel serves them directly without Express

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
-app.use(express.static(path.join(__dirname)));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
